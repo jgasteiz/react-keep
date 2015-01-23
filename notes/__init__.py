40de4c6051aeb9d255fb273de14ecc59a9a151b6
Javi Manzano <javi.manzano.oller@gmail.com>
2015-01-23 22:49:30 +0000

    Add postgresql database

--- notes/__init__.py
+++ notes/__init__.py
@@ -1,8 +1,12 @@
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
+if app.debug:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 from public import views
